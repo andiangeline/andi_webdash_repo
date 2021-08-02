554976d377d28eb6908466c6f2e48ed678533273
andiangeline <angeline.magna0729@gmail.com>
2021-08-02 17:09:29 -0400

    module 7 commit
    module 7 commit

---
+++ module7.py
@@ -0,0 +1,20 @@
+#module7
+
+#okteto login
+#okteto namespace
+#okteto up
+
+#source webdash2/bin/activate
+#python -m pip freeze > requirements.txt
+
+#pip install plotly_express
+#plotly_express can translate graphs into html snippets
+
+#pip install ninja2
+#ninja2 enables you to use python containers in html packages
+
+#pip install aiofiles
+
+#python -m pip install --upgrade pip
+
+#http://localhost:8080/
